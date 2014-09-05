a4b421b42bf16c773ad91619cdced6e50a61cd4a
cuiql <cuiqualong@gmail.com>
2014-09-05 17:06:24 +0800

    add TabButtonGroup class to manage TabButton
    Signed-off-by: cuiql <cuiqualong@gmail.com>

--- src/com/ray/custom/view/android/activity/TestActivity.java
+++ src/com/ray/custom/view/android/activity/TestActivity.java
@@ -4,52 +4,72 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.ray.custom.view.android.R;
 import com.ray.custom.view.android.widget.TabButton;
+import com.ray.custom.view.android.widget.TabButtonGroup;
+import com.ray.custom.view.android.widget.TabButtonGroup.OnCheckedChangeListener;
 
-public class TestActivity extends Activity implements OnClickListener {
-	private TabButton left;
-	private TabButton center;
-	private TabButton right;
+public class TestActivity extends Activity implements OnClickListener{
+	
+	private TabButtonGroup g;
 
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		left = (TabButton) findViewById(R.id.btn_left);
+		g = (TabButtonGroup) this.findViewById(R.id.tabGroup);
+
+		g.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+			@Override
+			public void onCheckedChanged(int checkedId) {
+				switch (checkedId) {
+				case R.id.btn_left:
+					
+					Toast.makeText(TestActivity.this, "Left", Toast.LENGTH_SHORT).show();
+					break;
+				case R.id.btn_center:
+					Toast.makeText(TestActivity.this, "Center", Toast.LENGTH_SHORT).show();
+					break;
+				case R.id.btn_right:
+					Toast.makeText(TestActivity.this, "Right", Toast.LENGTH_SHORT).show();
+					break;
+				}
+
+			}
+
+		});
+		
+		Button left = (Button)findViewById(R.id.left);
 		left.setOnClickListener(this);
-
-		center = (TabButton) findViewById(R.id.btn_center);
+		
+		Button center = (Button)findViewById(R.id.center);
 		center.setOnClickListener(this);
-
-		right = (TabButton) findViewById(R.id.btn_right);
+		
+		Button right = (Button)findViewById(R.id.right);
 		right.setOnClickListener(this);
 
-		resetState(R.id.btn_left);
 	}
 
+	@Override
 	public void onClick(View v) {
-		resetState(v.getId());
-	}
-
-	private void resetState(int id) {
-		// ｽｫﾈ�ｴﾅ･ｱｳｾｰﾉ靹ﾃﾎｪﾎｴﾑ｡ﾖﾐ
-		left.setSelected(false);
-		center.setSelected(false);
-		right.setSelected(false);
-		
-		// ｽｫｵ羹ﾄｰｴﾅ･ｱｳｾｰﾉ靹ﾃﾎｪﾒﾑﾑ｡ﾖﾐ
-		switch (id) {
-		case R.id.btn_left:
-			left.setSelected(true);
+		switch(v.getId()){
+		case R.id.left:
+			((TabButton)this.findViewById(R.id.btn_left)).setChecked(true);
 			break;
-		case R.id.btn_center:
-			center.setSelected(true);
+			
+		case R.id.center:
+			((TabButton)this.findViewById(R.id.btn_center)).setChecked(true);
 			break;
-		case R.id.btn_right:
-			right.setSelected(true);
+			
+		case R.id.right:
+			((TabButton)this.findViewById(R.id.btn_right)).setChecked(true);
 			break;
 		}
+		
 	}
+
 }
--- src/com/ray/custom/view/android/widget/TabButton.java
+++ src/com/ray/custom/view/android/widget/TabButton.java
@@ -2,20 +2,34 @@ package com.ray.custom.view.android.widget;
 
 
 
-import com.ray.custom.view.android.R;
-
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.widget.Button;
 
+import com.ray.custom.view.android.R;
+import com.ray.custom.view.android.widget.TabButtonGroup.OnChildCheckChangedListener;
+
 public class TabButton extends Button {
-	private int normal_bg_res;
-	private int selected_bg_res;
+	private int mNormalBackground;
+	private int mSelectBackgroud;
+	private boolean mChecked = false;
+	
+	private OnChildCheckChangedListener mListener;
 
 	public TabButton(Context context) {
 		super(context);
+		
+	}
+	
+	public boolean isChecked(){
+		return mChecked;
+	}
+	
+	public void setChecked(boolean checked){
+		mListener.onChanged(this,checked,false);
 	}
 
 	public TabButton(Context context, AttributeSet attrs) {
@@ -23,20 +37,36 @@ public class TabButton extends Button {
 
 		TypedArray typeArray = context.obtainStyledAttributes(attrs,
 				R.styleable.TabButton);
-		normal_bg_res = typeArray.getResourceId(
+		mNormalBackground = typeArray.getResourceId(
 				R.styleable.TabButton_normal_background, 0);
-		selected_bg_res = typeArray.getResourceId(
+		mSelectBackgroud = typeArray.getResourceId(
 				R.styleable.TabButton_selected_background, 0);
+		
+		
+		
 
 		typeArray.recycle();
 	}
-
-	public void setSelected(boolean selected) {
-		if (selected) {
-			setBackgroundResource(selected_bg_res);
+	
+	public void setOnChildCheckChangedListener(OnChildCheckChangedListener listener){
+		mListener = listener;
+	}
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {
+		
+		if(event.getAction() == MotionEvent.ACTION_UP){
+			mListener.onChanged(this,true,true);
+		}
+		return super.onTouchEvent(event);
+	}
+	
+	public void update(boolean checked){
+		if (checked) {
+			setBackgroundResource(mSelectBackgroud);
 			setTextColor(Color.WHITE);
 		} else {
-			setBackgroundResource(normal_bg_res);
+			setBackgroundResource(mNormalBackground);
 			setTextColor(Color.BLACK);
 		}
 	}
